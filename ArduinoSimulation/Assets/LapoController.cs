864006c0c6653e61c87fb1cda9022612a14ae921
miandilan <miandilan@gmail.com>
2024-10-09 20:05:50 -0400

    Text fun

--- ArduinoSimulation/Assets/LapoController.cs
+++ ArduinoSimulation/Assets/LapoController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.IO.Ports;
-
+using TMPro;
 
 public class LapoController : MonoBehaviour
 {
@@ -15,6 +15,9 @@ public class LapoController : MonoBehaviour
     public Transform armPart2;
     [SerializeField]
     public Transform armPart3;
+    [SerializeField]
+    public TMP_Text text1;
+    public TMP_Text text2;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,7 @@ public class LapoController : MonoBehaviour
         {
             try
             {
+                ShowMessageOne("Operation Started! Equipment Ready!");
                 string data = sPort.ReadLine();
                 string[] vals = data.Split(',');
 
@@ -43,14 +47,31 @@ public class LapoController : MonoBehaviour
                     armPart2.localRotation = Quaternion.Euler(angle2, 0, 0);
                     armPart3.localRotation = Quaternion.Euler(angle3, 0, 0);
                     Debug.Log("Moving...");
+                    
                 }
             }
 
             catch (TimeoutException)
             {
+                ShowMessageTwo("Operation Not Started! Equipment Inactive!");
                 Debug.LogWarning("Timeout reading serial port");
+                
             }
         }
+    }
+
+    public void ShowMessageOne(string msg)
+    {
+        text1.text = msg;
+        text1.gameObject.SetActive(true);
+        text2.gameObject.SetActive(false);
+    }
+
+    public void ShowMessageTwo(string msg)
+    {
+        text2.text = msg;
+        text2.gameObject.SetActive(true);
+        text1.gameObject.SetActive(false);
     }
 
     private void OnApplicationQuit()
